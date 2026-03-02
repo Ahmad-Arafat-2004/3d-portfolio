20e2b83a863ca22dabc4c3b87cb9fbbe75607098
Ahmad Arafat <144969677+Ahmad-Arafat-2004@users.noreply.github.com>
2026-03-02 11:37:49 +0300

    Update next.config.js for image optimization and SVG
    Enable unoptimized images and adjust SVG handling in webpack.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
   images: {
+    unoptimized: true,
     domains: [
       "res.cloudinary.com",
       "go-skill-icons.vercel.app",
@@ -9,29 +11,26 @@ const nextConfig = {
       "raw.githubusercontent.com",
     ],
   },
+
   webpack(config) {
-    // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule) =>
       rule.test?.test?.(".svg")
     );
 
     config.module.rules.push(
-      // Reapply the existing rule, but only for svg imports ending in ?url
       {
         ...fileLoaderRule,
         test: /\.svg$/i,
-        resourceQuery: /url/, // *.svg?url
+        resourceQuery: /url/,
       },
-      // Convert all other *.svg imports to React components
       {
         test: /\.svg$/i,
         issuer: /\.[jt]sx?$/,
-        resourceQuery: { not: /url/ }, // exclude if *.svg?url
+        resourceQuery: { not: /url/ },
         use: ["@svgr/webpack"],
       }
     );
 
-    // Modify the file loader rule to ignore *.svg, since we have it handled now.
     fileLoaderRule.exclude = /\.svg$/i;
 
     return config;
